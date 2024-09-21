08f87cc4c117f54fb71e1317061a1690e3fdd63e
ThaddeusBardsley <thaddeusbardsley2@gmail.com>
2024-09-21 09:58:10 -0400

    new drive code proxy issues

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain.java
@@ -2,6 +2,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
 //import static org.firstinspires.ftc.teamcode.Utilities.OpModeUtils.hardwareMap;
 //import static org.firstinspires.ftc.teamcode.Utilities.OpModeUtils.multTelemetry;
 //import static org.firstinspires.ftc.teamcode.Utilities.OpModeUtils.setOpMode;
@@ -27,16 +28,58 @@ public class Drivetrain {
     }
 
 
-    public void drive (double YPower, double XPower, double HeadingPower, double Speed){
+    public void drive (double YPower, double XPower, double HeadingPower,  double Slow){
 
-        //
-        fl.setPower(-(YPower - XPower + HeadingPower) * Speed);
-        //
-        fr.setPower((YPower + XPower - HeadingPower) * Speed);
-        //
-        bl.setPower(-(YPower + XPower + HeadingPower) * Speed);
-        //backwards
-        br.setPower((YPower - XPower - HeadingPower) * Speed);
+        if (Slow > 0.05) {
+            //
+            fl.setPower(-(YPower - XPower + HeadingPower) * 0.5);
+            //
+            fr.setPower((YPower + XPower - HeadingPower) * 0.5);
+            //
+            bl.setPower(-(YPower + XPower + HeadingPower) * 0.5);
+            //backwards
+            br.setPower((YPower - XPower - HeadingPower) * 0.5);
+        } else {
+            //
+            fl.setPower(-(YPower - XPower + HeadingPower) * 1);
+            //
+            fr.setPower((YPower + XPower - HeadingPower) * 1);
+            //
+            bl.setPower(-(YPower + XPower + HeadingPower) * 1);
+            //backwards
+            br.setPower((YPower - XPower - HeadingPower) * 1);
+        }
+
+        double YVector = YPower;
+        double XVector = XPower;
+        double HVector = HeadingPower;
+
+        telemetry.addData( "Y vector = ", + YVector);
+        telemetry.addData( "X vector = ", + XVector);
+        telemetry.addData( "Heading vector = ", + HVector);
+
+    }
+
+    public void driveDO(double drive, double strafe, double turn, double slow, double heading){
+        Vector2d driveVector = new Vector2d(strafe, drive);
+        Vector2d rotatedVector = driveVector.rotate(Math.toRadians(heading));
+
+        drive = rotatedVector.y;
+        strafe = rotatedVector.x;
+
+
+
+        if (slow > 0.05){
+            fl.setPower((drive + strafe + turn) * -0.5);
+            fr.setPower((drive - strafe - turn) * -0.5);
+            bl.setPower((drive - strafe + turn) * -0.5);
+            br.setPower((drive + strafe - turn) * -0.5);
+        } else {
+            fl.setPower((drive + strafe + turn) * -1);
+            fr.setPower((drive - strafe - turn) * -1);
+            bl.setPower((drive - strafe + turn) * -1);
+            br.setPower((drive + strafe - turn) * -1);
+        }
 
     }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PidDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PidDrive.java
@@ -2,22 +2,19 @@
 
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.IMU;
 
 
+@TeleOp(name="PidDrive", group="Iterative Opmode")
 
-@TeleOp(name="MakeMove", group="Iterative Opmode")
-
-public class MakeMove extends OpMode
+public class PidDrive extends OpMode
 {
     // Declare OpMode members.
     private Drivetrain drivetrain;
+    IMU gyro;
 
     /*
      * Code to run ONCE when the driver hits INIT
@@ -28,6 +25,14 @@ public class MakeMove extends OpMode
         telemetry.addData("Status", "init");
 
         drivetrain = new Drivetrain(hardwareMap);
+
+        gyro = hardwareMap.get(IMU .class, "imu");
+        // Adjust the orientation parameters to match your robot
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
+        gyro.initialize(parameters);
 
 
     }
@@ -48,6 +53,8 @@ public class MakeMove extends OpMode
 
     public void start() {
         telemetry.addData("Status", "start");
+
+        gyro.resetYaw();
 
     }
 
